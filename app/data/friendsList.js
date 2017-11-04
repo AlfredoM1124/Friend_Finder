1c345a759f5ab98d7b66ca183409f2b8ff54b8ef
Alfredo Munoz <AlfredoM@Alfredos-MBP.cfl.rr.com>
2017-11-03 21:51:32 -0400

    Added friends to friendsList.js, will add Pictures later

---
+++ app/data/friendsList.js
@@ -0,0 +1,62 @@
+var friendsList = [
+	{
+		"name":"Deshaun Watson",
+		"photo": "Insert URL HERE",
+		"scores": [
+			2,
+			5,
+			4,
+			6,
+			9,
+			7,
+			8,
+			9
+		]
+	},
+	{
+		"name":"Leonard Fournette",
+		"photo":"Insert URL Here",
+		"scores": [
+			8,
+			8,
+			9,
+			6,
+			9,
+			7,
+			3,
+			8
+		]
+	},
+	{
+		"name":"Michael Thomas",
+		"photo": "Insert URL HERE",
+		"scores": [
+			2,
+			5,
+			4,
+			6,
+			9,
+			7,
+			8,
+			9
+		]
+	},
+	{
+		"name":"Lonzo Ball",
+		"photo":"Insert URL Here",
+		"scores": [
+			3,
+			9,
+			4,
+			5,
+			3,
+			2,
+			1,
+			0,
+			4,
+			6
+		]
+	}
+];
+
+module.exports = friendsList;
